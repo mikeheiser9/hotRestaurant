b8a690b8f7aac19dc55def721ed890960c40d788
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-20 13:51:47 -0700

    added more routes

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ var path = require("path");
 var apiRoute = require("./backEnd/routes/apiRoutes.js");
 var htmlRoute = require("./backEnd/routes/htmlRoutes.js");
 var resturant = require("./backEnd/data/reservation.js");
+
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use("/", express.static('frontend'));
 
 console.log(resturant);
 apiRoute(app);
 htmlRoute(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
